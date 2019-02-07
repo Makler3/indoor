6221b31c343abd3ebd89fc498107029eb02e8ed1
Исмагил Сайфутдинов <ismagsg@gmail.com>
2019-02-07 12:18:11 +0300

    second floor objects added

--- IndoorNavigation/ViewController.swift
+++ IndoorNavigation/ViewController.swift
@@ -8,9 +8,9 @@
 
 import UIKit
 
-private var allRooms: [[Rooms]] = [[Rooms(comment: nil, polygon: "0 0 3 0 3 5 0 5", name: nil, type: 1), Rooms(comment: nil, polygon: "3 0 7 0 7 10 3 5", name: nil, type: 1), Rooms(comment: nil, polygon: "7 0 11 0 11 10 7 10", name: nil, type: 1)]]
+private var allRooms: [[Rooms]] = [[Rooms(comment: nil, polygon: "0 0 3 0 3 5 0 5", name: nil, type: 1), Rooms(comment: nil, polygon: "3 0 7 0 7 10 3 5", name: nil, type: 1), Rooms(comment: nil, polygon: "7 0 11 0 11 10 7 10", name: nil, type: 1)], [Rooms(comment: nil, polygon: "2 3 5 3 5 6 2 6", name: nil, type: 1), Rooms(comment: nil, polygon: "5 3 8 3 8 9 5 9", name: nil, type: 1)]]
 
-var allFloors: [Floors] = [Floors(roomsrelationship: NSSet(array: allRooms[0]), name: nil, comment: nil), Floors(roomsrelationship: nil, name: nil, comment: nil)]
+var allFloors: [Floors] = [Floors(roomsrelationship: NSSet(array: allRooms[0]), name: nil, comment: nil), Floors(roomsrelationship: NSSet(array: allRooms[1]), name: nil, comment: nil)]
 
 var allVertexes = [Vertex]()
 var allEdges = [Edge]()
@@ -48,18 +48,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         allVertexes.append(Vertex(coordinates: "2 2.5", comment: nil))
-        allVertexes.append(Vertex(coordinates: "4 2.5", comment: nil))
-        allVertexes.append(Vertex(coordinates: "6 1.5", comment: nil))
-        allVertexes.append(Vertex(coordinates: "8 1.5", comment: nil))
-        allVertexes.append(Vertex(coordinates: "6 5", comment: nil))
-        allVertexes.append(Vertex(coordinates: "8 5", comment: nil))
+        allVertexes.last!.roomsrelationship = allRooms[0][0]
         
-        allVertexes[0].roomsrelationship = allRooms[0][0]
-        allVertexes[1].roomsrelationship = allRooms[0][1]
-        allVertexes[2].roomsrelationship = allRooms[0][1]
-        allVertexes[3].roomsrelationship = allRooms[0][2]
-        allVertexes[4].roomsrelationship = allRooms[0][1]
-        allVertexes[5].roomsrelationship = allRooms[0][2]
+        allVertexes.append(Vertex(coordinates: "4 2.5", comment: nil))
+        allVertexes.last!.roomsrelationship = allRooms[0][1]
+        
+        allVertexes.append(Vertex(coordinates: "6 1.5", comment: nil))
+        allVertexes.last!.roomsrelationship = allRooms[0][1]
+        
+        allVertexes.append(Vertex(coordinates: "8 1.5", comment: nil))
+        allVertexes.last!.roomsrelationship = allRooms[0][2]
+        
+        allVertexes.append(Vertex(coordinates: "6 5", comment: nil))
+        allVertexes.last!.roomsrelationship = allRooms[0][1]
+        
+        allVertexes.append(Vertex(coordinates: "8 5", comment: nil))
+        allVertexes.last!.roomsrelationship = allRooms[0][2]
         
         allEdges.append(Edge(distance: 0, vertexfromrelationship: allVertexes[0], vertextorelationship: allVertexes[1], doorscoordinates: "3 2 3 3", comment: nil))
         allEdges.append(Edge(distance: 0, vertexfromrelationship: allVertexes[1], vertextorelationship: allVertexes[0], doorscoordinates: nil, comment: nil))
@@ -80,7 +84,20 @@ class ViewController: UIViewController {
         allEdges.append(Edge(distance: 100, vertexfromrelationship: allVertexes[1], vertextorelationship: allVertexes[4], doorscoordinates: nil, comment: nil))
         allEdges.append(Edge(distance: 100, vertexfromrelationship: allVertexes[4], vertextorelationship: allVertexes[1], doorscoordinates: nil, comment: nil))
         
-        mapView.pathVertexes = graph.findShortestPathRunningDijkstra(start: allVertexes[5], finish: allRooms[0][0]).1
+        allVertexes.append(Vertex(coordinates: "4.5 4.5", comment: nil))
+        allVertexes.last!.roomsrelationship = allRooms[1][0]
+        
+        allVertexes.append(Vertex(coordinates: "5.5 4.5", comment: nil))
+        allVertexes.last!.roomsrelationship = allRooms[1][1]
+        
+        allEdges.append(Edge(distance: 0, vertexfromrelationship: allVertexes[6], vertextorelationship: allVertexes[7], doorscoordinates: "5 4 5 5", comment: nil))
+        allEdges.append(Edge(distance: 0, vertexfromrelationship: allVertexes[7], vertextorelationship: allVertexes[6], doorscoordinates: "5 4 5 5", comment: nil))
+        
+        allEdges.append(Edge(distance: 0.5, vertexfromrelationship: allVertexes[0], vertextorelationship: allVertexes[6], doorscoordinates: nil, comment: nil))
+        allEdges.append(Edge(distance: 0.5, vertexfromrelationship: allVertexes[6], vertextorelationship: allVertexes[0], doorscoordinates: nil, comment: nil))
+        
+        
+        mapView.pathVertexes = graph.findShortestPathRunningDijkstra(start: allVertexes[5], finish: allRooms[1][1]).1
         mapView.needsPathBuild = true
     }
 
